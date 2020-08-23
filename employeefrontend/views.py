c43be7f7e5d637f22566366b1a6d49231b6710a2
Courtney Jacobs <cjacobs124@gmail.com>
2020-08-23 18:26:37 -0400

    On Logout

---
+++ employeefrontend/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+#from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib import messages
+#from .forms import SignUpForm, EditProfileForm
+
+#LOGIN
+def login_home(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None: #If the user exists
+			login(request, user)
+			messages.success(request, ('You Are Logged in'))
+			return redirect('main')
+		else:
+			messages.success(request, ('Error Logging In - Please Try Again...'))
+			return redirect('login')
+	else:
+		return render(request, "authenticate/login.html", {})
+
+
+#MAIN
+def main(request):
+	return render(request, "authenticate/main.html", {})
